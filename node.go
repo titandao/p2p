fb3c507ebc710ef2050563f96d363890bcb304fc
Misrab M. Faizullah-Khan <faizullah.misrab@gmail.com>
2016-04-10 17:45:27 +0800

    commit

--- node.go
+++ node.go
@@ -2,71 +2,106 @@ package p2p
 
 
 import (
-	"net"
+	"os"
+	"fmt"
+	// "net"
 	"time"
+
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/p2p"
 )
 
 
-type File struct {
-	Path string
-	Hash string
-}
 
 
-type Node struct {
-	// list of file hashes it has fully downloaded
-	Files []File
-}
+func Listen() {
+	nodekey, _ := crypto.GenerateKey()
 
 
+	println("Starting p2p server...")
 
-
-
-func Listen(conns chan *net.Conn, timeout int) error {
-	// , quit chan struct{}
-	l, err := net.ListenTCP("tcp", nil)
-	if err != nil { return err }
-	defer l.Close()
-
-	if timeout > 0 {
-		l.SetDeadline(time.Now().Add(time.Duration(timeout) * time.Second))
+	srv := p2p.Server{
+	    MaxPeers:   10,
+	    PrivateKey: nodekey,
+	    Name:       "my node name",
+	    ListenAddr: ":30300",
+	    Protocols:  []p2p.Protocol{},
 	}
 
+	if err := srv.Start(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
 
-	for {
-		conn, err := l.Accept()
-		if err != nil { return err }
-		conns <- &conn
-	}
+    setInterval(func() {
+    	println("ticking...")	
+	})
 
-
-	// for {
-	// 	select {
-	// 	case <- quit:
-	// 		return nil // TODO cleanup
-	// 	default:
-	// 		conn, err := l.Accept()
-	// 		if err != nil { return err }
-	// 		conns <- &conn
-	// 	}	
-	// }
-
-
-
-	// go func() {
-	// 	select {
-	// 	case <- quit:
-	// 		l.Close()
-	// 	}
-	// }()
-
-	// // defer l.Close()
-	// // println(l.Addr().String())
-
-	// return l.Addr().String(), nil
+    select {}
 }
 
 
 
+func setInterval(f func()) chan struct{} {
+	ticker := time.NewTicker(5 * time.Second)
+	quit := make(chan struct{})
+	go func() {
+	    for {
+	       select {
+	        case <- ticker.C:
+	            // do stuff
+	            f()
+	        case <- quit:
+	            ticker.Stop()
+	            return
+	        }
+	    }
+ 	}()
+
+ 	return quit
+}
 
 
+
+const messageId = 0
+
+type Message string
+
+func MyProtocol() p2p.Protocol {
+    return p2p.Protocol{
+        Name:    "MyProtocol",
+        Version: 1,
+        Length:  1,
+        Run:     msgHandler,
+    }
+}
+
+func msgHandler(peer *p2p.Peer, ws p2p.MsgReadWriter) error {
+    for {
+
+        msg, err := ws.ReadMsg()
+        if err != nil {
+            return err
+        }
+
+        var myMessage Message
+        err = msg.Decode(&myMessage)
+        if err != nil {
+            // handle decode error
+            continue
+        }
+
+        switch myMessage {
+        case "foo":
+            err := p2p.SendItems(ws, messageId, "bar")
+            if err != nil {
+                return err
+            }
+        default:
+            fmt.Println("recv:", myMessage)
+        }
+    }
+
+    return nil
+}
--- node_test.go
+++ node_test.go
@@ -5,23 +5,12 @@ import (
 	"testing"
 
 	// "time"
-	"net"
+	// "net"
 )
 
 
 
 
 func TestListen(t *testing.T) {
-	conns := make(chan *net.Conn)
-	// q := make(chan struct{})
-
-	err := Listen(conns, 5)
-
-	if err, ok := err.(*net.OpError); ok && err.Timeout() {
-		println("timed out")
-	} else if err != nil{ t.Fatal(err) }
-
-
-	// time.Sleep(1 * time.Second)
-	// q <- struct{}{}
+	Listen()
 }
